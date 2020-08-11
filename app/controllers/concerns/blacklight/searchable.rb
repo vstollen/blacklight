691e6caf9abba49d37a74731d0264462cf548524
Justin Coyne <jcoyne@justincoyne.com>
2020-08-10 22:16:21 -0500

    Move search_service_class to Searchable
    Anything that uses Searchable should be able to configure a search_service_class, this does not have anything to do with a Controller.

--- app/controllers/concerns/blacklight/searchable.rb
+++ app/controllers/concerns/blacklight/searchable.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
 module Blacklight::Searchable
+  extend ActiveSupport::Concern
+
+  included do
+    # Which class to use for the search service. You can subclass SearchService if you
+    # want to override any of the methods (e.g. SearchService#fetch)
+    class_attribute :search_service_class
+    self.search_service_class = Blacklight::SearchService
+  end
+
   # @return [Blacklight::SearchService]
   def search_service
     search_service_class.new(config: blacklight_config, user_params: search_state.to_h, **search_service_context)
